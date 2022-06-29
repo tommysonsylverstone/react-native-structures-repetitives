97854f1c85276aade233d93d291ff1250838164b
btenreiro <tenreiro.benjamin@gmail.com>
2022-06-29 17:23:57 +0200

    fini integer et pgcd

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Integer from './integer';
+import Pgcd from './pgcd';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Integer />
+      <Pgcd />
     </View>
   );
 }
